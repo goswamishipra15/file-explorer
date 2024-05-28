40e33f212e7faa0b0ed4b724720f5a738c8fcf19
Shipra Goswami <goswamishipra246@gmail.com>
2024-05-28 11:56:21 +0530

    file explorer like vs code file explorer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import explorer from './data/folderData';
+import Folder from './components/Folder';
+import useTraverseTree from "./hooks/use-traverse-tree";
 
-function App() {
+export default function App() {
+  const [explorerData, setExplorerData] = useState(explorer);
+
+  const { insertNode } = useTraverseTree();
+
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item, isFolder);
+    setExplorerData(finalTree);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Folder handleInsertNode={handleInsertNode} explorer={explorerData} />
     </div>
   );
 }
-
-export default App;
